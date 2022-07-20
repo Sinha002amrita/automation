f7bd020c19b0fda4242f19a6ee3dfea323055cfd
Amrita <ask4am11@gmail.com>
2022-07-20 19:54:27 +0530

    basic auth Broken Images Challenging DOM Checkboxes Context Menu Digest Authentication Disappearing Elements

--- examples.java
+++ examples.java
@@ -1,10 +1,14 @@
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
 
 public class examples {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\vk sinha\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -22,9 +26,47 @@ public class examples {
         driver.navigate().back();
         driver.findElement(By.linkText("Basic Auth")).click();
 
-        //driver.switchTo().alert().sendKeys("admin");
+        driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().back();
+        driver.navigate().back();
+
+        driver.findElement(By.linkText("Broken Images")).click();
 
         driver.navigate().back();
 
+        driver.findElement(By.partialLinkText("Check")).click();
+        // to select
+        driver.findElement(By.xpath("//input[@type='checkbox'] [1]")).click();
+        //to deselect
+        driver.findElement(By.xpath("//input[@type='checkbox'] [2]")).click();
+
+        driver.navigate().back();
+
+        driver.findElement(By.linkText("Context Menu")).click();
+        Actions act = new Actions(driver);
+        act.contextClick(driver.findElement(By.id("hot-spot"))).perform();
+        driver.switchTo().alert().accept();
+
+        driver.navigate().back();
+
+        driver.findElement(By.partialLinkText("Digest")).click();
+        driver.get("http://admin:admin@the-internet.herokuapp.com/digest_auth");
+
+        driver.navigate().back();
+        driver.navigate().back();
+
+        driver.findElement(By.linkText("Disappearing Elements")).click();
+
+        System.out.println(driver.findElement(By.xpath("//div[@id='content']")).getText());
+        //refesh page
+        driver.navigate().refresh();
+        System.out.println("After refreshing/reloading page");
+        System.out.println(driver.findElement(By.xpath("//div[@id='content']")).getText());
+
+        driver.navigate().back();
     }
 }
+
+

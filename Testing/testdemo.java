717a3b4be95a22ceb834ca9b5083187645486044
Amrita <ask4am11@gmail.com>
2022-08-09 17:33:57 +0530

    links in new tab

--- Testing/testdemo.java
+++ Testing/testdemo.java
@@ -2,27 +2,30 @@ package Testing;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebElement;
+import java.util.List;
 
 public class testdemo extends ProjectSetup {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
         ProjectSetup p = new ProjectSetup();
         driver = p.openBrowser();
 
         //print no.of links present in page
- /*       System.out.println(driver.findElements(By.xpath("//body/div[2]/div/ul/li")).size());
+       System.out.println(driver.findElements(By.xpath("//body/div[2]/div/ul/li")).size());
         //alternate way to print no.of links present in page
         System.out.println(driver.findElements(By.tagName("li")).size());
-*/
-        //to open links of same page in different tab
 
-        for(int i=1; i<driver.findElements(By.tagName("li")).size(); i++);
+        //to open links of same page in different tab
+        List<WebElement> linkCollection = driver.findElements(By.tagName("a"));
+         System.out.println(driver.findElement(By.tagName("ul")).getText());
+
+        for (int i = 1; i < linkCollection.size(); i++)
         {
             String clickOnLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
-            driver.findElements(By.tagName("li")).get(i).sendKeys(clickOnLink);
-        }
+            linkCollection.get(i).sendKeys(clickOnLink);
 
+        }
     }
+
 }
